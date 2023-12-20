71e521586c189ce0b53e11c23c8236482966d0d7
Damian Brzana <brzanad@gmail.com>
2023-12-20 11:32:20 +0100

    diofeq.m added

---
+++ diofeq.m
@@ -0,0 +1,24 @@
+function [X,Y]=diofeq(A,B,C,nX,nY)
+    % Funkcja rozwiazuje rownanie diofantyczne AX+BY=C
+    A = A(:); 
+    B = B(:); 
+    C = C(:);
+    nA = length(A) - 1;
+    nB = length(B) - 1;
+    nC = length(C) - 1;
+    C = [C; zeros(nX+nY+2-nC-1, 1)];
+    nC = length(C) - 1;
+    A_mtx = zeros(nX+nY+2, nX+nY+2);
+    if (nC+1) ~= (nX+nY+2); X = []; Y = []; return; end
+    for i = 1:nX+1
+        A_mtx(i:nA+i, i) = A;
+    end
+    ss=1;
+    for i = nX+2:nX+nY+2
+        A_mtx(ss:nB+ss, i) = B;
+        ss = ss + 1;
+    end
+    Z = A_mtx \ C;
+    X = Z(1:nX+1)';
+    Y = Z(nX+2:nX+nY+2)';
+end
